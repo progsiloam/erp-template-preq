c12ee99ae989ce8465efa2ef32aa2cc9d113a22a
progsiloam <pg33@adiconsulting.co>
2024-09-18 09:07:15 +0700

    [feat] slicing page pre-q-validity page

---
+++ src/views/dashboard/config/pre-q-validity/DialogHistoryMajorOfBussiness.vue
@@ -0,0 +1,81 @@
+<template>
+  <BaseDialog title="Update History" width="600">
+    <template v-slot:default>
+      <ShgTable :columns="columns" :items="items"> </ShgTable>
+    </template>
+    <template v-slot:actions>
+      <div class="d-flex ga-3">
+        <BaseButton @click="handleCloseDialog" color="primary">Okay</BaseButton>
+      </div>
+    </template>
+  </BaseDialog>
+</template>
+
+<script setup lang="ts">
+import { BaseButton, BaseDialog, ShgTable } from '@siloamhospitals/erp-template-vuetify-components';
+import { closeDialog } from 'vue3-promise-dialog';
+
+// const questionList = ref<Question[]>([]);
+// const selectedQuestions = ref<Question[]>([]);
+// const searchText = ref<string>('');
+
+const columns = [
+  {
+    label: 'Update ID',
+    value: 'update_id',
+  },
+  {
+    label: 'Latest Update By',
+    value: 'latest_update_by',
+  },
+  {
+    label: 'Last Change Date',
+    value: 'last_change_date',
+  },
+];
+
+const items = [
+  {
+    update_id: 'TP0001',
+    latest_update_by: 'Gilang',
+    last_change_date: '2021-10-10',
+  },
+];
+
+const handleCloseDialog = () => {
+  closeDialog(null);
+};
+
+// const handleRelease = () => {
+//   closeDialog(null);
+// };
+
+// const onCheckBoxChange = (e: Event, item: Question) => {
+//   const target = e.target as HTMLInputElement;
+
+//   if (target.checked) {
+//     selectedQuestions.value.push(item);
+//   } else {
+//     selectedQuestions.value = selectedQuestions.value.filter((q) => q._id !== item._id);
+//   }
+// };
+
+// const isCheckBoxChecked = (item: Question) => {
+//   return selectedQuestions.value.some((q) => q._id === item._id);
+// };
+
+// const fetchQuestion = async (query: string) => {
+//   const res = await fetchQuestionsPaginate({ questionText: query });
+//   questionList.value = res.data;
+// };
+
+// onMounted(() => {
+//   fetchQuestion('');
+// });
+
+// watch([searchText], async ([newSearchText]) => {
+//   const search = newSearchText ?? '';
+//   // fetchQuestion(search);
+//   selectedQuestions.value = [];
+// });
+</script>
---
+++ src/views/dashboard/config/pre-q-validity/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <h3 class="mb-5">Pre-Qualification Validity</h3>
+  <ShgTable :columns="columns" :items="items">
+    <template v-slot:action="{ item }">
+      <BaseButtonIcon color="warning" size="small" icon="mdi-pen" class="mx-1" @click="handleEdit(item._id)" />
+      <BaseButtonIcon color="primary" size="small" icon="mdi-history" class="mx-1" @click="handleHistory(item._id)" />
+    </template>
+  </ShgTable>
+</template>
+
+<script setup lang="ts">
+import { BaseButtonIcon, ShgTable } from '@siloamhospitals/erp-template-vuetify-components';
+import { openDialog } from 'vue3-promise-dialog';
+import DialogEditMajorOfBusiness from './DialogEditMajorOfBusiness.vue';
+import DialogHistoryMajorOfBussiness from './DialogHistoryMajorOfBussiness.vue';
+
+const columns = [
+  {
+    label: 'Major of Bussiness',
+    field: 'major_of_business',
+  },
+  {
+    label: 'Validity Periods (Days)',
+    field: 'validity_periods',
+  },
+  {
+    label: 'Action',
+    field: 'action',
+  },
+];
+
+const items = [
+  {
+    _id: 1,
+    major_of_business: 'Major of Bussiness 1',
+    validity_periods: '30',
+  },
+  {
+    _id: 2,
+    major_of_business: 'Major of Bussiness 2',
+    validity_periods: '60',
+  },
+  {
+    _id: 3,
+    major_of_business: 'Major of Bussiness 3',
+    validity_periods: '90',
+  },
+];
+
+const handleHistory = async (_id: number) => {
+  const data = await openDialog(DialogHistoryMajorOfBussiness);
+  if (!data) return;
+};
+
+const handleEdit = async (_id: number) => {
+  const data = await openDialog(DialogEditMajorOfBusiness);
+  if (!data) return;
+};
+</script>
