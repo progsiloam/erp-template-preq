a9db3e3f3ab41abf611e6747e35742e334d51e4d
farhanadi2024 <pg32@adiconsulting.co>
2024-09-23 20:20:52 +0700

    make question language can change dynamically

--- src/views/dashboard/vendor/submit/MajorTab.vue
+++ src/views/dashboard/vendor/submit/MajorTab.vue
@@ -12,7 +12,7 @@
           <ShgTextField
             v-model="values.sections[Sindex].questions[Qindex].user_answer[0]"
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             :required="question.is_required"
             v-if="question.input_type === 'Short answer'"
           />
@@ -25,14 +25,14 @@
               }))
             "
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             :required="question.is_required"
             v-if="question.input_type === 'Checkboxes'"
           />
           <ShgDatePicker
             v-model="values.sections[Sindex].questions[Qindex].user_answer[0]"
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             :required="question.is_required"
             v-if="question.input_type === 'Date'"
           />
@@ -45,7 +45,7 @@
               }))
             "
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             :required="question.is_required"
             v-if="question.input_type === 'Dropdown'"
           />
@@ -53,14 +53,14 @@
             v-model="values.sections[Sindex].questions[Qindex].user_answer"
             :required="question.is_required"
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             v-if="question.input_type === 'File Upload (expiry)'"
           />
           <PreQFileUpload
             v-model="values.sections[Sindex].questions[Qindex].user_answer[0]"
             :required="question.is_required"
             :label="question.question_text[langStore.activeLang]"
-            :field-name="`${question.question_text}+${Qindex}`"
+            :field-name="`${values.name}-section[${Sindex}]-questions[${Qindex}]`"
             v-if="question.input_type === 'File Upload'"
           />
         </template>
@@ -87,7 +87,7 @@ import {
   ShgForm,
   ShgTextField,
 } from '@siloamhospitals/erp-template-vuetify-components';
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, toRaw, watch } from 'vue';
 import type { SubmissionAttributes } from './IData';
 
 const props = defineProps<{ questionnaire: SubmissionAttributes['questionnaires'][number] }>();
@@ -109,4 +109,42 @@ onMounted(() => {
 onUnmounted(() => {
   console.log('unMounted ' + props.questionnaire.name);
 });
+
+watch(
+  () => langStore.activeLang,
+  (newLang, oldLang) => {
+    let newValues = toRaw(values.value);
+
+    newValues.sections = newValues.sections.map((section) => {
+      section.questions = section.questions.map((question) => {
+        const isQuestionHaveOption = question.options && question.options.length > 0;
+
+        if (!isQuestionHaveOption) {
+          return question;
+        }
+
+        const alreadyAnswered = question.user_answer.length > 0;
+
+        if (!alreadyAnswered) {
+          return question;
+        }
+
+        question.user_answer = question.user_answer.map((ans) => {
+          const option = question.options.find((opt) => {
+            const text = opt.option_text[oldLang];
+            return text === ans;
+          });
+
+          return option ? option.option_text[newLang] : ans;
+        });
+
+        return question;
+      });
+
+      return section;
+    });
+
+    values.value.sections = newValues.sections;
+  },
+);
 </script>
